573f501d963174cf78df28208f19341e1560c210
IamDevTrivedi <devtrivedi22082005@gmail.com>
2026-01-08 18:18:31 +0530

    add: include script to transfer GeoLite2-City.mmdb to build directory

---
+++ scripts/transfer-geofile.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+# Run this script from the project root directory
+# Usage: copies the ./server/src/contants/GeoLite2-City.mmdb to ./server/build/contants/GeoLite2-City.mmdb
+
+cp ./server/src/constants/GeoLite2-City.mmdb ./server/build/constants/GeoLite2-City.mmdb
+echo "GeoLite2-City.mmdb has been copied to the build directory."
