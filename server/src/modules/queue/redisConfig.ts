abb1197520b46222f2ee72e65eaebff55c585d55
IamDevTrivedi <devtrivedi22082005@gmail.com>
2026-03-06 00:14:55 +0530

    fix: rm dependecy on LOCAL_REDIS env var

--- server/src/modules/queue/redisConfig.ts
+++ server/src/modules/queue/redisConfig.ts
@@ -1,14 +1,9 @@
 import { config } from "@config/env";
 import { ConnectionOptions } from "bullmq";
 
-export const redisConfig: ConnectionOptions = config.LOCAL_REDIS
-    ? {
-          host: "localhost",
-          port: 6379,
-      }
-    : {
-          host: config.REDIS_HOST,
-          port: config.REDIS_PORT,
-          username: config.REDIS_USERNAME,
-          password: config.REDIS_PASSWORD,
-      };
+export const redisConfig: ConnectionOptions = {
+    port: config.REDIS_PORT,
+    username: config.REDIS_USERNAME,
+    password: config.REDIS_PASSWORD,
+    host: config.REDIS_HOST,
+}
