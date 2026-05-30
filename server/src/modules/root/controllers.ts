5c2782f2ec3a74e1531809220fd523dff536ba46
IamDevTrivedi <devtrivedi22082005@gmail.com>
2026-05-30 16:55:16 +0530

    TESTING PIPELINES

--- server/src/modules/root/controllers.ts
+++ server/src/modules/root/controllers.ts
@@ -4,7 +4,7 @@ import { Request, Response } from "express";
 export const controllers = {
     index: (req: Request, res: Response) => {
         return sendResponse(res, {
-            message: "Welcome to the Trimium API! - Root Endpoint - Changed - 3",
+            message: "Welcome to the Trimium API! - Root Endpoint - Changed - 4",
         });
     },
 };
