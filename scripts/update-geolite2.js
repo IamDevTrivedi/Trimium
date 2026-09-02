883e0fe1cebe9f7616f00b4d4197d561f08230d7
IamDevTrivedi <devtrivedi22082005@gmail.com>
2026-09-03 03:46:03 +0530

    refactor: simplify GeoLite2 update script

--- scripts/update-geolite2.js
+++ scripts/update-geolite2.js
@@ -11,31 +11,23 @@ const TEMP_DIR = path.join(__dirname, "..", "temp");
 const TEMP_FILE = path.join(TEMP_DIR, "GeoLite2-City.tar.gz");
 const TARGET_DIR = path.join(__dirname, "..", "server", "src", "constants");
 const TARGET_FILE = path.join(TARGET_DIR, "GeoLite2-City.mmdb");
-const DIST_TARGET_DIR = path.join(__dirname, "..", "server", "dist", "constants");
-const DIST_TARGET_FILE = path.join(DIST_TARGET_DIR, "GeoLite2-City.mmdb");
 
 try {
     if (!LICENSE_KEY) {
         throw new Error("MAXMIND_LICENSE_KEY environment variable is not set!");
     }
 
-    await $`mkdir -p ${TEMP_DIR} ${TARGET_DIR} ${DIST_TARGET_DIR}`.quiet();
+    await $`mkdir -p ${TEMP_DIR} ${TARGET_DIR}`.quiet();
     await $`curl -L -o ${TEMP_FILE} ${DOWNLOAD_URL}`.quiet();
     await $`tar -xzf ${TEMP_FILE} -C ${TEMP_DIR}`.quiet();
+
     const findResult = await $`find ${TEMP_DIR} -name "*.mmdb" -print -quit`.quiet();
     const mmdbPath = findResult.stdout.toString().trim();
     if (!mmdbPath) {
         throw new Error("MMDB file not found in archive");
     }
 
-    const targetExists = await $`test -f ${TARGET_FILE}`.quiet().then(() => true).catch(() => false);
-    if (targetExists) {
-        await $`cp ${TARGET_FILE} ${TARGET_FILE}.backup`.quiet();
-    }
-
-    await $`cp ${mmdbPath} ${TARGET_FILE}`.quiet();
-    await $`cp ${mmdbPath} ${DIST_TARGET_FILE}`.quiet();
-
+    await $`cp -f ${mmdbPath} ${TARGET_FILE}`.quiet();
     await $`rm -rf ${TEMP_DIR}`.quiet();
     console.log("Success: GeoLite2-City database updated.");
     process.exit(0);
